a0315ad0770f5cfe38485dce0d8b351590781c81
estevesnp <nuno.pinto@live.com>
2024-12-01 10:56:40 +0000

    zig - 2024 day 1

---
+++ 2024/day-1/zig/part-1/main.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+const List = std.ArrayList(u32);
+
+pub fn main() !void {
+    const file = try std.fs.cwd().openFile("../../input.txt", .{});
+    defer file.close();
+
+    const reader = file.reader();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    var left_list = List.init(allocator);
+    var right_list = List.init(allocator);
+
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try parseLine(line, &left_list, &right_list);
+    }
+
+    std.mem.sort(u32, left_list.items, {}, std.sort.asc(u32));
+    std.mem.sort(u32, right_list.items, {}, std.sort.asc(u32));
+
+    var sum: u32 = 0;
+    for (left_list.items, right_list.items) |left, right| {
+        sum += if (left > right) left - right else right - left;
+    }
+
+    std.debug.print("RESULT: {}\n", .{sum});
+}
+
+fn parseLine(line: []const u8, left_list: *List, right_list: *List) !void {
+    var iter = std.mem.tokenizeAny(u8, line, " ");
+
+    const left = iter.next() orelse @panic("no left found");
+    const right = iter.next() orelse @panic("no right found");
+
+    const left_num = std.fmt.parseInt(u32, left, 10) catch @panic("couldnt parse left");
+    const right_num = std.fmt.parseInt(u32, right, 10) catch @panic("couldnt parse right");
+
+    try left_list.append(left_num);
+    try right_list.append(right_num);
+}
---
+++ 2024/day-1/zig/part-2/main.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const file = try std.fs.cwd().openFile("../../input.txt", .{});
+    defer file.close();
+
+    const reader = file.reader();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    var map = std.AutoHashMap(u32, u32).init(allocator);
+    var right_list = std.ArrayList(u32).init(allocator);
+
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try parseLine(line, &map, &right_list);
+    }
+
+    var sum: u32 = 0;
+    for (right_list.items) |num| {
+        sum += num * (map.get(num) orelse 0);
+    }
+
+    std.debug.print("RESULT: {}\n", .{sum});
+}
+
+fn parseLine(
+    line: []const u8,
+    map: *std.AutoHashMap(u32, u32),
+    right_list: *std.ArrayList(u32),
+) !void {
+    var iter = std.mem.tokenizeAny(u8, line, " ");
+
+    const left = iter.next() orelse @panic("no left found");
+    const right = iter.next() orelse @panic("no right found");
+
+    const left_num = std.fmt.parseInt(u32, left, 10) catch @panic("couldnt parse left");
+    const right_num = std.fmt.parseInt(u32, right, 10) catch @panic("couldnt parse right");
+
+    const count = map.get(left_num) orelse 0;
+    try map.put(left_num, count + 1);
+    try right_list.append(right_num);
+}
